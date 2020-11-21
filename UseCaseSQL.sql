ca97b6aa2a5c5d2fb96ee2f12656763591ff87fe
Ryan <ressenma@uncc.edu>
2020-11-21 15:54:14 -0500

    All the SQL for the UseCase Diagram

---
+++ UseCaseSQL.sql
@@ -0,0 +1,41 @@
+SELECT 
+    driver_id, AVG(driver_rating)
+FROM
+    `order`,
+    ratings
+GROUP BY driver_id
+
+SELECT 
+    driver_id, MIN(driver_rating)
+FROM
+    `order`,
+    ratings
+GROUP BY driver_id
+
+SELECT 
+    driver_id, MAX(driver_rating)
+FROM
+    `order`,
+    ratings
+GROUP BY driver_id
+
+SELECT 
+    restaurant_id, AVG(restaurant_rating)
+FROM
+    `order`,
+    ratings
+GROUP BY restaurant_id
+
+SELECT 
+    restaurant_id, MIN(restaurant_rating)
+FROM
+    `order`,
+    ratings
+GROUP BY restaurant_id
+
+SELECT 
+    restaurant_id, MAX(restaurant_rating) AS MaxRating
+FROM
+    `order`,
+    ratings
+GROUP BY restaurant_id
