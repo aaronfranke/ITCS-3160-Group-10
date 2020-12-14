433e2a35d26b6052412ef2e87089a1b0e8a1139c
Scott Girard <sgirard@uncc.edu>
2020-12-14 02:34:24 -0500

    add stored procedures

---
+++ sql/calculate_driver_stored_procedure.sql
@@ -0,0 +1,27 @@
+DELIMITER //
+
+CREATE PROCEDURE Calculate_Driver(in id_of_driver int)
+BEGIN
+	SELECT
+    driver_id, AVG(driver_rating)
+FROM
+    `order`,
+    ratings
+WHERE driver_id = id_of_driver;
+SELECT
+    driver_id, MAX(driver_rating)
+FROM
+    `order`,
+    ratings
+WHERE driver_id = id_of_driver;
+SELECT
+    driver_id, MIN(driver_rating)
+FROM
+    `order`,
+    ratings
+WHERE driver_id = id_of_driver;
+END //
+
+DELIMITER ;
+
+
---
+++ sql/calculate_restaurant_stored_procedure.sql
@@ -0,0 +1,25 @@
+DELIMITER //
+
+CREATE PROCEDURE Calculate_Restaurant(in id_of_restaurant int)
+BEGIN
+	SELECT
+    restaurant_id, AVG(restaurant_rating)
+FROM
+    `order`,
+    ratings
+WHERE restaurant_id = id_of_restaurant;
+SELECT
+    restaurant_id, MIN(restaurant_rating)
+FROM
+    `order`,
+    ratings
+WHERE restaurant_id = id_of_restaurant;
+SELECT
+    restaurant_id, MAX(restaurant_rating)
+FROM
+    `order`,
+    ratings
+WHERE restaurant_id = id_of_restaurant;
+END //
+
+DELIMITER ;
